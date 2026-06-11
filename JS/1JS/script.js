35844646aed663127c2d4e3da904ae63f6389468
Gaurav Kachawa <gauravkachawa16@email.com>
2026-06-11 15:39:19 +0530

    Javascript

---
+++ JS/1JS/script.js
@@ -0,0 +1,41 @@
+console.log("Samiran is good boy");
+let Samiran = "Yashwant";
+console.log("Samiran");
+
+Samiran = "Das";
+console.log(Samiran);
+
+Samiran = 20;
+console.log(`${Samiran} sep`)
+
+
+const Yashwant = "Samiran";
+console.log(Yashwant);
+
+
+// Block Scope and global scope in javascript
+
+let a =80;
+console.log(a);
+
+var b = 20;
+console.log(b);
+
+const c = 55;
+console.log(c);
+
+
+{
+    let a = 30;
+    console.log(a)
+
+    var b = 40;
+    console.log(b)
+
+    const c = 75;
+    console.log(c)
+}
+
+console.log(a)
+console.log(b)
+console.log(c)
